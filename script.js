ec3545f7dad12a3ec7ece5b8feda9ded87f0fea5
Tochid <113685578+Tochid@users.noreply.github.com>
2023-09-15 11:56:51 +0000

    opdracht 13 h2

--- script.js
+++ script.js
@@ -1,32 +1,69 @@
+var aantalRijenRaster = 12;
+var aantalKolommenRaster = 18;
+var celGrootte;
+
 var animatie = [];
-var aantalBeeldjes = 3;
-var nummer = 0;
+var aantalBeeldjes = 6;
+var nummer = 3;
+
+var frame;
+var xJos = 400;
+var yJos = 300;
 
 function preload() {
-  for (var b = 0;b < aantalBeeldjes;b++) {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+  frame = loadImage("images/sprites/Jos100px/Jos_" + nummer +".png");
+  
+   for (var b = 0;b < aantalBeeldjes;b++) {
     nieuw_beeldje = loadImage("images/sprites/Jos_losse_beeldjes/Jos-" + b +".png");
     animatie.push(nieuw_beeldje);
   }
 }
 
 function setup() {
-  canvas = createCanvas(460,460);
+  canvas = createCanvas(900,600);
   canvas.parent();
-  noStroke();
-  frameRate(500);
-  textFont("Georgia");
-  textSize(18);
+  frameRate(11);
+  celGrootte = width / aantalKolommenRaster;
 }
 
 function draw() {
-  background('lavender');
-  image(animatie[nummer],0,0);
-  nummer++;
-
-  if (nummer == aantalBeeldjes) {
-    nummer = 0;
+  background(brug);
+  tekenRaster();
+   
+  
+  if (keyIsDown(LEFT_ARROW)) {
+   xJos -= celGrootte;
+   nummer = 3;
+ }
+  
+  if (keyIsDown(RIGHT_ARROW)) {
+    xJos += celGrootte;
+     nummer = 4;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    yJos-=celGrootte;
+     nummer = 1;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    yJos += celGrootte;
+     nummer = 0;
   }
   
-  text("frameCount=" + frameCount,5,20);
-  text("nummer=" + nummer,5,40);
+  xJos = constrain(xJos,0,width - celGrootte);
+  yJos = constrain(yJos,0,height - celGrootte);
+  
+   image(animatie[nummer],xJos,yJos,celGrootte,celGrootte);
+}
+
+function tekenRaster() {
+  push();
+  noFill();
+  stroke('grey');
+  for (var rij = 0;rij < aantalRijenRaster;rij++) {
+    for (var kolom = 0;kolom < aantalKolommenRaster;kolom++) {
+      rect(kolom*celGrootte,rij*celGrootte,celGrootte,celGrootte);
+    }
+  }
+  pop();
 }
