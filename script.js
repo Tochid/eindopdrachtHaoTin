508f00dedbb64c015c162cef31d2e225872ff591
Tochid <113685578+Tochid@users.noreply.github.com>
2023-09-22 12:19:49 +0000

    opdracht 33 h2

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
-var raster = {
-  aantalRijen: 6,
-  aantalKolommen: 9,
-  celGrootte: null,
-  
+class rasterclass  {
+  constructor(rij,kolom){
+  this.aantalRijen= rij;
+ this.aantalKolommen= kolom;
+ this.celGrootte= null;
+  }
   berekenCelGrootte() {
     this.celGrootte = canvas.width/this.aantalKolommen;
-  },
+  };
   teken() {
     push();
     noFill();
     stroke('grey');
-    for (rij=0;rij<this.aantalRijen;rij++) {
-      for (kolom=0;kolom<this.aantalKolommen;kolom++) {
+    for (var rij=0;rij<this.aantalRijen;rij++) {
+      for (var kolom=0;kolom<this.aantalKolommen;kolom++) {
         rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
       }
     }
@@ -19,15 +20,16 @@ var raster = {
   }
 }
 
-var jos = {
-  x: 400,
-  y: 300,
-  animatie: [],
-  aantalFrames: 6,
-  frameNummer: 3,
-  stapGrootte: null,
-  gehaald: false,
-
+class Jos {
+  constructor() {
+    this.x = 400;
+    this.y = 300;
+    this.animatie = [];
+    this.frameNummer =  3;
+    this.stapGrootte = null;
+    this.gehaald = false;
+  }
+  
   beweeg() {
     if (keyIsDown(LEFT_ARROW)) {
       this.x -= this.stapGrootte;
@@ -47,33 +49,34 @@ var jos = {
     }
     
     this.x = constrain(this.x,0,canvas.width);
-    this.y = constrain(this.y,0,canvas.height-raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
     
     if (this.x == canvas.width) {
       this.gehaald = true;
     }
-  },
+  }
   
-  wordtGeraakt(v) {
-    if (this.x == v.x && this.y == v.y) {
+  wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
       return true;
     }
     else {
       return false;
     }
-  },
+  }
   
   toon() {
     image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
   }
 }
 
-
-var alice = {
-  x: 700,
-  y: 200,
-  sprite: null,
-  stapGrootte: null,
+class Vijand {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
 
   beweeg() {
     this.x += floor(random(-1,2))*this.stapGrootte;
@@ -81,47 +84,65 @@ var alice = {
 
     this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
     this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
-  },
+  }
   
   toon() {
     image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
   }
 }
 
-
 function preload() {
   brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
-  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
-  for (var b = 0;b < jos.aantalFrames;b++) {
-    frameJos = loadImage("images/sprites/Jos100px/Jos_" + b + ".png");
-    jos.animatie.push(frameJos);
-  }
 }
 
 function setup() {
   canvas = createCanvas(900,600);
   canvas.parent();
   frameRate(10);
+  textFont("Verdana");
+  textSize(90);
+  
+  raster = new rasterclass(6,9);
   raster.berekenCelGrootte();
-  jos.stapGrootte = 1*raster.celGrootte;
-  alice.stapGrootte = 1*raster.celGrootte;
+  
+  eve = new Jos();
+  eve.stapGrootte = 1*raster.celGrootte;
+  for (var b = 0;b < 6;b++) {
+    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    eve.animatie.push(frameEve);
+  }
+    bob = new Vijand(700,200);
+  bob.stapGrootte = 1*eve.stapGrootte;
+  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png");
+  
+  
+  alice = new Vijand(700,200);
+  alice.stapGrootte = 1*eve.stapGrootte;
+  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
 }
+
 
 function draw() {
   background(brug);
   raster.teken();
-  jos.beweeg();
+  eve.beweeg();
+  bob.beweeg();
   alice.beweeg();
-  jos.toon();
+  eve.toon();
   alice.toon();
-  if (jos.wordtGeraakt(alice)) {
+  bob.toon();
+  
+  if (eve.wordtGeraakt(alice)) {
     noLoop();
   }
-    if (jos.gehaald) {
-    background("green");
-    fill("white");
-    textSize(200);
-    text("geegee", 30,300);
+   if (eve.wordtGeraakt(bob)) {
+    noLoop();
+  }
   
+  if (eve.gehaald) {
+    background('green');
+    fill('white');
+    text("GEEEEEEGEEEEEE",30,300);
+    noLoop();
   }
 }
