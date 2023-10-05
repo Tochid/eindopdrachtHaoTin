b23cc780ae3df027f73f3de62008fa394d2af61a
Tochid <113685578+Tochid@users.noreply.github.com>
2023-10-05 10:48:53 +0000

    original commit

--- script.js
+++ script.js
@@ -1,50 +1,147 @@
-class Knikker {
-  constructor() {
-    this.diameter = 40;
-    this.straal = this.diameter / 2;
-    this.x = random(this.straal,canvas.width - this.straal);
-    this.y = random(this.straal,canvas.height - this.straal);
-    this.snelheidX = random(1,10);
-    this.snelheidY = random(1,10);
+class Raster {
+  constructor(r,k) {
+    this.aantalRijen = r;
+    this.aantalKolommen = k;
+    this.celGrootte = null;
   }
-
-  beweeg() {
-    this.x += this.snelheidX;
-    this.y += this.snelheidY;
-    
-    if (this.x < this.straal || this.x > canvas.width - this.straal) {
-      this.snelheidX *= -1;
-    }
-    if (this.y < this.straal || this.y > canvas.height - this.straal) {
-      this.snelheidY *= -1;
-    }
+  
+  berekenCelGrootte() {
+    this.celGrootte = canvas.width / this.aantalKolommen;
   }
   
   teken() {
-    fill(255,255,255,1);
-    ellipse(this.x,this.y,this.diameter);
+    push();
+    noFill();
+    stroke('grey');
+    for (var rij = 0;rij < this.aantalRijen;rij++) {
+      for (var kolom = 0;kolom < this.aantalKolommen;kolom++) {
+        rect(kolom*this.celGrootte,rij*this.celGrootte,this.celGrootte,this.celGrootte);
+      }
+    }
+    pop();
   }
 }
 
-var knikkerVerzameling = [];
+class Jos {
+  constructor() {
+    this.x = 400;
+    this.y = 300;
+    this.animatie = [];
+    this.frameNummer =  3;
+    this.stapGrootte = null;
+    this.gehaald = false;
+  }
+  
+  beweeg() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.x -= this.stapGrootte;
+      this.frameNummer = 2;
+    }
+    if (keyIsDown(RIGHT_ARROW)) {
+      this.x += this.stapGrootte;
+      this.frameNummer = 1;
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.y -= this.stapGrootte;
+      this.frameNummer = 4;
+    }
+    if (keyIsDown(DOWN_ARROW)) {
+      this.y += this.stapGrootte;
+      this.frameNummer = 5;
+    }
+    
+    this.x = constrain(this.x,0,canvas.width);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+    
+    if (this.x == canvas.width) {
+      this.gehaald = true;
+    }
+  }
+  
+  wordtGeraakt(vijand) {
+    if (this.x == vijand.x && this.y == vijand.y) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  toon() {
+    image(this.animatie[this.frameNummer],this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}  
+
+class Vijand {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+    this.sprite = null;
+    this.stapGrootte = null;
+  }
+
+  beweeg() {
+    this.x += floor(random(-1,2))*this.stapGrootte;
+    this.y += floor(random(-1,2))*this.stapGrootte;
+
+    this.x = constrain(this.x,0,canvas.width - raster.celGrootte);
+    this.y = constrain(this.y,0,canvas.height - raster.celGrootte);
+  }
+  
+  toon() {
+    image(this.sprite,this.x,this.y,raster.celGrootte,raster.celGrootte);
+  }
+}
+
+function preload() {
+  brug = loadImage("images/backgrounds/dame_op_brug_1800.jpg");
+}
 
 function setup() {
-  canvas = createCanvas(1000,300);
+  canvas = createCanvas(900,600);
   canvas.parent();
-  frameRate(236);
-  colorMode(RGB,255,255,255,1);
-  background(0,0,75,1);
-  noStroke();
-  k1 = new Knikker();
-  for (var b = 0; b < 10; b++) {
-    knikkerVerzameling.push(new Knikker());
+  frameRate(10);
+  textFont("Verdana");
+  textSize(90);
+  
+  raster = new Raster(6,9);
+  
+  raster.berekenCelGrootte();
+  
+  eve = new Jos();
+  eve.stapGrootte = 1*raster.celGrootte;
+  for (var b = 0;b < 6;b++) {
+    frameEve = loadImage("images/sprites/Eve100px/Eve_" + b + ".png");
+    eve.animatie.push(frameEve);
   }
+  
+  alice = new Vijand(700,200);
+  alice.stapGrootte = 1*eve.stapGrootte;
+  alice.sprite = loadImage("images/sprites/Alice100px/Alice.png");
+
+  bob = new Vijand(600,400);
+  bob.stapGrootte = 1*eve.stapGrootte;
+  bob.sprite = loadImage("images/sprites/Bob100px/Bob.png");  
 }
 
 function draw() {
-  background(0,0,75,0.2);
-  for (var n = 0; n < knikkerVerzameling.length; n++) {
-    knikkerVerzameling[n].beweeg();
-    knikkerVerzameling[n].teken();
+  background(brug);
+  raster.teken();
+  eve.beweeg();
+  alice.beweeg();
+  bob.beweeg();
+  eve.toon();
+  alice.toon();
+  bob.toon();
+  
+  if (eve.wordtGeraakt(alice) || eve.wordtGeraakt(bob)) {
+    noLoop();
+  }
+  
+  if (eve.gehaald) {
+    background('green');
+    fill('white');
+    text("Je hebt gewonnen!",30,300);
+    noLoop();
   }
 }
